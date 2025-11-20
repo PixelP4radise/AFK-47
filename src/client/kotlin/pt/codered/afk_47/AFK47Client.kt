2d92178b0f41536bafe10c4ea9e770d21191190f
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-11-20 18:28:45 +0000

    Adds basic command structure
    Adds a basic command structure for the mod.
    
    Registers the "afk" command with subcommands: "chop",
    "mine", "fish", "kill", and "stop".  Currently, only the
    "chop" subcommand has functionality, sending a message to
    the player.  The other subcommands are placeholders.
    
    Also adds a dependency for `nether-pathfinder`.

--- src/client/kotlin/pt/codered/afk_47/AFK47Client.kt
+++ src/client/kotlin/pt/codered/afk_47/AFK47Client.kt
@@ -1,9 +1,47 @@
 package pt.codered.afk_47
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.minecraft.text.Text
 
 object AFK47Client : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    override fun onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+
+            // The base command is "/afk"
+            dispatcher.register(
+                literal("afk")
+
+                    // Subcommand: /afk chop
+                    .then(literal("chop").executes { context ->
+                        val client = context.source.client
+                        client.player?.sendMessage(Text.of("Chop"), false)
+                        1 // Return 1 indicates success
+                    })
+
+                    // Subcommand: /afk mine
+                    .then(literal("mine").executes { context ->
+                        val client = context.source.client
+                        1
+                    })
+
+                    // Subcommand: /afk fish
+                    .then(literal("fish").executes { context ->
+                        1
+                    })
+
+                    // Subcommand: /afk kill
+                    .then(literal("kill").executes { context ->
+                        1
+                    })
+
+                    // Subcommand: /afk stop
+                    .then(literal("stop").executes { context ->
+                        1
+                    })
+            )
+        }
+    }
 }
