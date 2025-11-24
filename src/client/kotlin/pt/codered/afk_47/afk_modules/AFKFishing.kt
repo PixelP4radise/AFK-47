71027650cec24869524ce5a5d836b5fdcc482850
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-11-24 23:47:21 +0000

    Adds initial AFK fishing module
    Introduces a new AFK module for fishing.
    
    This commit sets up the basic structure for automatically
    fishing, including handling messages and checking if the
    player is holding a fishing rod.

--- src/client/kotlin/pt/codered/afk_47/afk_modules/AFKFishing.kt
+++ src/client/kotlin/pt/codered/afk_47/afk_modules/AFKFishing.kt
@@ -1,30 +1,38 @@
 package pt.codered.afk_47.afk_modules
 
 import net.minecraft.client.MinecraftClient
+import net.minecraft.item.Items
 import net.minecraft.text.Text
-import net.minecraft.util.math.Vec3d
 import pt.codered.afk_47.utils.ChatUtils
 
 object AFKFishing : AFKModule() {
-    private var anchorPos: Vec3d? = null
-    private var anchorYaw: Float = 0f
-    private var anchorPitch: Float = 0f
 
 
     override fun onEnable(client: MinecraftClient) {
-        val player = client.player ?: return
-
-        anchorPos = player.pos
-        anchorYaw = player.yaw
-        anchorPitch = player.pitch
+        //reset variables
+        //save position
     }
 
     override fun onTick(client: MinecraftClient) {
-
+        if (!isHoldingRod(client)) return
+        //check if player is holding a cane
+        //check if bobber is already on water
+        //if not send cane after delay from reel in
+        //if yes wait for entity with name ? save then check when entity name becomes !!!
     }
+
 
     override fun onSystemMessage(client: MinecraftClient, message: Text) {
         if (ChatUtils.isDefenseMessage(message) || ChatUtils.isManaMessage(message)) return
         println("Got unfiltered system message: ${message.string}")
+    }
+
+    override fun onChat(client: MinecraftClient, message: Text) {
+        println("Got chat message: ${message.string}")
+    }
+
+    private fun isHoldingRod(client: MinecraftClient): Boolean {
+        val player = client.player ?: return false
+        return player.mainHandStack.item != Items.FISHING_ROD
     }
 }
